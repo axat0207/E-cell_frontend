7d8eba74ea255cd8bf7d75d642490af7b1936023
DhananjaySarathe <dhanajaysarathe26@gmail.com>
2023-09-11 08:23:56 +0530

    amuminiSectionDone

--- app/pages/Alumni.tsx
+++ app/pages/Alumni.tsx
@@ -1,8 +1,77 @@
-export default function Alumni(){
-    return(
-        
-        <>
-        <div>Alumni</div>
-        </>
+"use client"
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+import Image from "next/image";
+import { useEffect } from 'react';
+
+export default function Initiative() {
+    useEffect(() => {
+        AOS.init();
+    }, [])
+
+    return (
+        <div className="bg-black min-h-screen ">
+            <div className="text-3xl py-16 font-bold text-white text-center" data-aos="fade-up" data-aos-duration="1000">ALUMINI</div>
+            <div className="flex flex-col md:flex-row justify-center items-center md:gap-10 px-4 md:px-20 text-red-600" data-aos="fade-up">
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+            </div>
+            <div className="flex flex-col md:flex-row justify-center items-center md:gap-10 px-4 md:px-20 text-red-600" data-aos="fade-up">
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+                {/* <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div> */}
+            </div>
+        </div>
     )
 }
--- app/pages/PastEvents.tsx
+++ app/pages/PastEvents.tsx
@@ -25,3 +25,39 @@
 //         </>
 //     )
 // }
+
+"use client"
+import AOS from 'aos';
+import 'aos/dist/aos.css'
+import Image from "next/image";
+import { useEffect } from 'react';
+
+export default function PastEvents() {
+    useEffect(() => {
+        AOS.init();
+    }, [])
+
+    return (
+        <div className="min-h-screen ">
+            <div className="text-4xl font-semibold py-16 text-black text-center" data-aos="fade-up" data-aos-duration="1000">Past Events</div>
+            <div className="flex flex-col md:flex-row justify-center items-center md:gap-10 px-4 md:px-20 text-red-600" data-aos="fade-up">
+                {/* <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div> */}
+                {/* <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div> */}
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+                <div data-aos="flip-up" data-aos-offset="300" data-aos-duration="1000">
+                    <div className="text-center py-4">E-Summit</div>
+                    <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
