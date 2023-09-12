afb89f5eb7f17119c41a6b36b76569b4f423aa6a
DhananjaySarathe <dhanajaysarathe26@gmail.com>
2023-09-12 11:46:01 +0530

    Alumini and other Dj

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
-import About from "./pages/About";
-import Alumni from "./pages/Alumni";
-import Contact from "./pages/Contact";
-import Initiative from "./pages/Initiative";
-import Landing from "./pages/Landing";
-import Partner from "./pages/Partner";
-import PastEvents from "./pages/PastEvents";
-import RecentEvents from "./pages/RecentEvents";
-import UpcomingEvents from "./pages/UpcomingEvents";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import About from "../components/pages/About";
+import Alumni from "../components/pages/Alumni";
+import Contact from "../components/pages/Contact";
+import Initiative from "../components/pages/Initiative";
+import Landing from "../components/pages/Landing";
+import Partner from "../components/pages/Partner";
+import PastEvents from "../components/pages/PastEvents";
+import RecentEvents from "../components/pages/RecentEvents";
+import UpcomingEvents from "../components/pages/UpcomingEvents";
 
 
 export default function Home() {
